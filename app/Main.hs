f92a15c3ce1e0b8451582209bb6edc44c830d111
eb <->
2024-11-17 12:59:44 +0100

    Refactor loop state to a data type

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,9 @@ import ExprParser (eval)
 import Numeric (showFFloat)
 import System.Console.Haskeline (InputT, defaultSettings, getInputLine, outputStrLn, runInputT)
 
+-- State that holds the current key and the history (map from keys values)
+data LoopState = LS Char (Map.Map Char Double)
+
 main :: IO ()
 main = runInputT defaultSettings $ do
   outputStrLn $
@@ -15,26 +18,28 @@ main = runInputT defaultSettings $ do
         "==================================",
         "For usage information enter 'help'"
       ]
-  loop 'a' Map.empty
+  loop initState
+  where
+    initState = LS 'a' Map.empty
 
 -- Loops until the user quits
-loop :: Char -> Map.Map Char Double -> InputT IO ()
-loop key history = do
+loop :: LoopState -> InputT IO ()
+loop state = do
   input <- getInputLine ">> "
   case input of
     Nothing -> return ()
     Just cmd | cmd `elem` ["q", "quit", "Q", "exit"] -> return ()
-    Just "help" -> outputStrLn usage >> loop key history
+    Just "help" -> outputStrLn usage >> loop state
     Just expr -> do
-      let expr' = replaceLetters history expr
-      let (result, history', key') =
-            maybe ("Invalid expression", history, key) (updateState history key) $ eval expr'
+      let expr' = replaceLetters state expr
+      let (result, state') =
+            maybe ("Invalid expression", state) (updateState state) $ eval expr'
       outputStrLn result
-      loop key' history'
+      loop state'
 
 -- Replaces key to the value that the key represents
-replaceLetters :: Map.Map Char Double -> String -> String
-replaceLetters history = concatMap replace
+replaceLetters :: LoopState -> String -> String
+replaceLetters (LS _key history) = concatMap replace
   where
     replace c
       | validChar c = maybe [c] showNumber $ Map.lookup c history
@@ -42,8 +47,8 @@ replaceLetters history = concatMap replace
     showNumber d = ' ' : showFFloat Nothing d ""
 
 -- Updates history, key and the result of evaluating the input
-updateState :: Map.Map Char Double -> Char -> Double -> (String, Map.Map Char Double, Char)
-updateState history key expr = (resultAndKey, history', key')
+updateState :: LoopState -> Double -> (String, LoopState)
+updateState (LS key history) expr = (resultAndKey, LS key' history')
   where
     resultAndKey = result' ++ replicate len ' ' ++ ['[', key, ']']
     result'
